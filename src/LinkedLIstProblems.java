785a7031a42b745eff368ed95f9b2fa3d8a197df
Sam K Varghese <ksam071@gmail.com>
2023-01-21 22:58:43 +0530

    UC2 logic for adding elements

--- src/LinkedLIstProblems.java
+++ src/LinkedLIstProblems.java
@@ -1,15 +1,47 @@
-import java.util.LinkedList;
+class Node<T>{
+    T data;
+    Node nextNode = null;
+
+    Node(T data) {
+        this.data = data;
+    }
+}
+
+class LinkedList<T> {
+    Node<T> head = null;
+
+    public void addNode(T data) {
+        if(head == null) {
+            // linked list is empty
+            head = new Node(data);
+        } else {
+            Node curr = head;
+            while(curr.nextNode != null) {
+                curr = curr.nextNode;
+            }
+            curr.nextNode = new Node(data);
+        }
+    }
+
+    public String toString() {
+        Node curr = head;
+        String res = "[";
+        while(curr != null) {
+            res = res +  String.valueOf(curr.data) + ",";
+            curr = curr.nextNode;
+        }
+        res += "]";
+        return res;
+    }
+}
 
 public class LinkedLIstProblems {
 
     public static void main(String[] args) {
-        LinkedList numbers = new LinkedList();
-
-        numbers.add(56);
-        numbers.add(30);
-        numbers.add(70);
-
-
-        System.out.println(numbers);
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.addNode(30);
+        list.addNode(50);
+        list.addNode(70);
+        System.out.println(list);
     }
 }
