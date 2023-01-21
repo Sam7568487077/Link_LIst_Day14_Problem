9f40abfc19d6e8c47a837d9b3ae156d389511173
Sam K Varghese <ksam071@gmail.com>
2023-01-21 18:58:02 +0530

    UC1 add elements to linked list

---
+++ src/LinkedLIstProblems.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class LinkedLIstProblems {
+
+    public static void main(String[] args) {
+        LinkedList numbers = new LinkedList();
+
+        numbers.add(56);
+        numbers.add(30);
+        numbers.add(70);
+
+
+        System.out.println(numbers);
+    }
+}
